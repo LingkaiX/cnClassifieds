b6a673eb53c036a714c777f43d74fbedc6327d1d
Lingkai.Xu <xlk910728@gmail.com>
2017-08-18 11:07:27 +1000

    Add Google Analytics

--- footer-home.php
+++ footer-home.php
@@ -27,6 +27,16 @@
 <script src=<?php echo get_template_directory_uri() .'/js/myjs.js' ?>></script>
 <!-- 由于Geo WP 插件加载了：
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCQWClP6rOf55if8uN7jXIs_K2gheMECSw&libraries=places&callback=initMap&language=en-us" async defer></script>-->
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-104883243-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<!-- Google Analytics -->
 <?php wp_footer(); ?>
 </body>
 </html>
