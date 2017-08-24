63232b0c579c37a252cc21aac25062678ec0788a
Lingkai.Xu <xlk910728@gmail.com>
2017-08-24 11:41:41 +1000

    Change the order to load css files

--- functions.php
+++ functions.php
@@ -21,19 +21,19 @@ function SearchFilter($query) {
 add_filter('pre_get_posts','SearchFilter');
 //Enqueue scripts and styles.
 function add_styles_and_scripts() {
-	wp_enqueue_style( 'easylife-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'layui-css', get_template_directory_uri() . '/layui/css/layui.css', array(), '2.0.1', 'all' );
 	wp_enqueue_style( 'bootstrap-styles', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.7', 'all' );
 	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '4.7.0', 'all' );
 	//wp_enqueue_style( 'select2bs-css', get_template_directory_uri() . '/css/select2-bootstrap.min.css', array('select2-css'), '0.1.0', 'all' );
 	//wp_enqueue_style( 'select2-css', get_template_directory_uri() . '/css/select2.min.css', array(), '4.0.3', 'all' );
-	wp_enqueue_style( 'layui-css', get_template_directory_uri() . '/layui/css/layui.css', array(), '2.0.1', 'all' );
-	
-	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.7', true );
+	wp_enqueue_style( 'easylife-style', get_stylesheet_uri() );	
+
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.7', false );
 	//wp_enqueue_script( 'select2-js', get_template_directory_uri() . '/js/select2.min.js', array('jquery'), '4.0.3', true );
 	//wp_enqueue_script( 'select2-js-cn', get_template_directory_uri() . '/js/i18n/zh-CN.js', array('select2-js'), '4.0.3', true );
-	wp_enqueue_script( 'layui-js', get_template_directory_uri() . '/layui/layui.js', array(), '2.0.1', true );
-	wp_enqueue_script( 'my-js', get_template_directory_uri() . '/js/myjs.js', array('jquery','layui-js'), '1.0.1', true );
+	wp_enqueue_script( 'layui-js', get_template_directory_uri() . '/layui/layui.js', array(), '2.0.1', false );
+	wp_enqueue_script( 'my-js', get_template_directory_uri() . '/js/myjs.js', array('jquery','layui-js'), '1.0.1', false );
 	
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
--- js/myjs.js
+++ js/myjs.js
@@ -3,3 +3,11 @@ jQuery(document).ready(function($){
     var form = layui.form; //只有执行了这一步，部分表单元素才会自动修饰成功
   }); 
 }); 
+
+// jQuery(document).ready(function($){
+//   jQuery('#cateselect').select2({
+//       placeholder: "选择服务分类",
+//       language: "zh-CN",
+//       //theme: "bootstrap"
+//   });
+// });
