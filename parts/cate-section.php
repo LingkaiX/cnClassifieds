85ca50acfa60db5fa432b036f7bb16d389d33797
Lingkai.Xu <xlk910728@gmail.com>
2017-09-05 11:41:17 +1000

    Fix that bug again

--- parts/cate-section.php
+++ parts/cate-section.php
@@ -5,7 +5,7 @@
             foreach ($catesindex as $cate){
                 $catecount++;
                 echo '<div class="col-md-3 col-sm-3 col-xs-4 cate-col">';
-                    echo '<div class="row">';
+                    //echo '<div class="row">';
                         // $category_id = get_category_by_slug( $cate["slug"] );
                         // print_r($category_id);
                         // $href=get_category_link( $category_id->term_id );
@@ -15,13 +15,13 @@
                                 echo '<img class="home-cate-logo" src="'.get_template_directory_uri().'/img/'.$cate["imgsrc"].'"></img>';
                                 echo '<div class="clearfix"></div>';
                                 echo '<h4 class="cate-h">'.$cate["name"].'</h4>';
-                        echo '</a></div><div class="row">';
+                        echo '</a>';
                             echo '<div class="col-md-12 hidden-xs sub-item">';
                             foreach($cate["subcates"] as $subcate){
                                 $href=get_site_url()."/category/".$subcate["slug"];
                                 echo '<a href="'.$href.'"><p>'.$subcate["name"].'</p></a>';
                             }
-                echo '</div></div></div></div>';
+                echo '</div></div></div>';
                 if($catecount%4==0) echo '<div class="clearfix hidden-xs"></div>';    
             }
         ?>
