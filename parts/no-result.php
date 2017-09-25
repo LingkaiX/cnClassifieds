904d843fbf71b5c0d81711ace80f361699864ffa
Lingkai.Xu <xlk910728@gmail.com>
2017-09-25 17:49:01 +1000

    Add .search-no-result-container

--- parts/no-result.php
+++ parts/no-result.php
@@ -1,2 +1,2 @@
 <!-- <h4 class="bg-danger search-no-result">不好意思！我们没有找到你要的结果。</h4> -->
-<img src="<?php echo get_template_directory_uri();?>/img/page404.png" class="search-no-result"></img>
+<div class="search-no-result-container"><img src="<?php echo get_template_directory_uri();?>/img/page404.png" class="search-no-result"></img></div>
