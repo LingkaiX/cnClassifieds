502ac303424dc8d8d990fb82f31985907c3634a6
Lingkai.Xu <xlk910728@gmail.com>
2017-10-03 16:45:44 +1100

    Polish enquiry form style

--- parts/enquiry-form.php
+++ parts/enquiry-form.php
@@ -1,17 +1,24 @@
 <!-- muast be used in loop -->
 <?php if(get_post_meta($post->ID,'email-to-business',true)){ ?>
 <div class="enquiry-form-container" id="<?php echo 'enquiry-form-container-'.$post->ID ?>">
-    <div class="enquiry-form-header">
-        <button class="btn-close" onClick="closeEnquiry('<?php echo $post->ID ?>')"><i class="fa fa-times" aria-hidden="true"></i></button>
-    </div>
-    <div class="enquiry-form-body">
-        <form action=<?php echo get_site_url().'/wp-json/cnx/v1/mailtobusiness/'.$post->ID?> id="<?php echo 'enquiry-form-'.$post->ID ?>" onSubmit="return submitEnquiry(this,'<?php echo $post->ID ?>')" method="post">
-            <input type="text" id="search-item" name="name" class="" placeholder="姓名" autocomplete="off">
-            <input type="email" id="search-item" name="mail" class="" placeholder="邮箱" autocomplete="off">
-            <input type="tel" id="search-item" name="phone" class="" placeholder="电话" autocomplete="off">
-            <textarea name="enquiry" cols="40" rows="10" class="" aria-invalid="false" placeholder="内容"></textarea>
-            <button type="submit" class="btn btn-default btn-send">发送</button>
-        </form>
+    <div class="enquiry-form-content">
+        <div class="enquiry-form-header">
+            <span><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp;联系商家</span>
+            <button class="btn-close" onClick="closeEnquiry('<?php echo $post->ID ?>')"><i class="fa fa-times" aria-hidden="true"></i></button>
+        </div>
+        <div class="enquiry-form-body">
+            <form action=<?php echo get_site_url().'/wp-json/cnx/v1/mailtobusiness/'.$post->ID?> id="<?php echo 'enquiry-form-'.$post->ID ?>" onSubmit="return submitEnquiry(this,'<?php echo $post->ID ?>')" method="post">
+                <label for="">姓名（必填）:</label>
+                <input type="text" name="name" class="form-control input-text" autocomplete="off">
+                <label for="">邮箱（必填）:</label>
+                <input type="email" name="mail" class="form-control input-text" autocomplete="off">
+                <label for="">电话：</label>
+                <input type="tel" name="phone" class="form-control input-text" autocomplete="off">
+                <label for="">咨询内容（必填）:</label>
+                <textarea name="enquiry" cols="40" rows="10" class="form-control input-text" aria-invalid="false"></textarea>
+                <div><button type="submit" class="btn btn-default btn-send">发送</button></div>
+            </form>
+        </div>
     </div>
 </div>
 <button class="btn enquiry-btn" id="<?php echo 'enquiry-btn-'.$post->ID ?>" onClick="openEnquiry('<?php echo $post->ID ?>')">联系商家</button>
