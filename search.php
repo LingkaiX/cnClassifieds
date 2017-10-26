710fea6bff1a5c7e01c9d9f2ad7bdc5a012306f2
Lingkai.Xu <xlk910728@gmail.com>
2017-10-26 14:12:07 +1100

    Commit debug info

--- search.php
+++ search.php
@@ -14,7 +14,7 @@
 			<?php include 'parts/cate-list.php';?>
 		</div>
 		<main class="col-md-7 col-sm-9 col-xs-12"> <!-- listing box-->
-			<?php print_r($wp_query);
+			<?php 
 				if ( have_posts() ){
 					while ( have_posts() ) : 
 						the_post();
