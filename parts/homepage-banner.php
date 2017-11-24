5f475dc5a4d8eaf2a8ddd692476db6250027a37a
wjxsophia <31834741+wjxsophia@users.noreply.github.com>
2017-11-24 16:05:13 +1100

    top banner

--- parts/homepage-banner.php
+++ parts/homepage-banner.php
@@ -1,12 +1,12 @@
-<div id="hp-banner" style="position:fixed; top:0; left:0; right:0; min-height:50px; background-color:dimgray; display:none;z-index:999; text-align:center;">
-    <span > 联系我们 <i class="fa fa-phone" aria-hidden="true"></i> 1300 638 939 或 <a href="https://auads.com.au/publish-busines">免费发布信息</a></span>
+<div id="hp-banner">
+    <span class="top-banner-info"> 联系我们 <i class="fa fa-phone" aria-hidden="true"></i> 1300 638 939 或 <a href="https://auads.com.au/publish-busines">免费发布信息</a></span>
 </div>
 <script>
     //pasted from http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
     function getScrollXY() {
         var scrOfX = 0, scrOfY = 0;
         if( typeof( window.pageYOffset ) == 'number' ) {
-            //Netscape compliant
+            //Netscape compliant 
             scrOfY = window.pageYOffset;
             scrOfX = window.pageXOffset;
         } else if( document.body && ( document.body.scrollLeft || document.body.scrollTop ) ) {
