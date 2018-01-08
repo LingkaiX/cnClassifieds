7603e73c1ee52a1ac312aef0f7f49423eaa90ffe
Lingkai.Xu <xlk910728@gmail.com>
2018-01-08 14:43:07 +1100

    //

--- parts/cn-conversation-link.php
+++ parts/cn-conversation-link.php
@@ -2,8 +2,8 @@
 $url='';
 if (parsePath($_SERVER['REQUEST_URI'],'',-1)=='zh-tw'){
     $url=home_url().substr($_SERVER['REQUEST_URI'],6);
-    echo '<a href='.$url.' class="btn cnc-link">简体</a>';
+    echo '<a href='.$url.' class="btn cc-link">简体</a>';
 }else{
     $url=home_url().'/zh-tw'.$_SERVER['REQUEST_URI'];
-    echo '<a href='.$url.' class="btn cnc-link">繁体</a>';
+    echo '<a href='.$url.' class="btn cc-link">繁体</a>';
 }
