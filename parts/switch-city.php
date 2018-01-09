b65a0a4d8ffe37daa364e5ed85947ad49163b936
Lingkai.Xu <xlk910728@gmail.com>
2018-01-09 16:56:38 +1100

    Fix bug of last commit

--- parts/switch-city.php
+++ parts/switch-city.php
@@ -9,7 +9,22 @@
     <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         切换城市<span class="caret"></span></button>
     <ul class="dropdown-menu">
+    <?php 
+        if(is_search()){ 
+            $melUrl='?lat=-37.8152065&long=144.963937&s='.$_GET['s'];
+            $sdyUrl='?lat=-33.8688197&long=151.20929550000005&s='.$_GET['s'];        
+    ?>
         <li><a href="<?php echo $baseUrl.$reqUrl.$melUrl?>">墨尔本</a></li>
         <li><a href="<?php echo $baseUrl.$reqUrl.$sdyUrl?>">悉尼</a></li>
-    </ul>
+
+    <?php  
+        }else{
+            $melUrl='?lat=-37.8152065&long=144.963937';
+            $sdyUrl='?lat=-33.8688197&long=151.20929550000005';
+    ?>
+        <li><a href="<?php echo $baseUrl.$reqUrl.$melUrl?>">墨尔本</a></li>
+        <li><a href="<?php echo $baseUrl.$reqUrl.$sdyUrl?>">悉尼</a></li>
+
+    <?php   } ?>
+</ul>
 </div>
