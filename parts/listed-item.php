c2a18ac2466204ab1c4fdac4c29b3808dd6155a3
Lingkai.Xu <xlk910728@gmail.com>
2018-02-27 15:52:40 +1100

    //listed-items

--- parts/listed-item.php
+++ parts/listed-item.php
@@ -30,7 +30,7 @@
                                 if($mypost!=null){
                                     if(!empty($mypost->phone)) echo '<p><i class="ionicon ion-ios-telephone-outline" aria-hidden="true"></i><span>'.$mypost->phone.'</span></p>';
                                     if(!empty($mypost->email)) echo '<p><i class="ionicon ion-ios-email-outline" aria-hidden="true"></i><span>'.$mypost->email.'</span></p>';
-                                    if(!empty($mypost->website)) echo '<p><i class="ionicon ion-ios-world-outline" aria-hidden="true"></i><a target="_blank" href="'.$mypost->website.'">'.removeScheme($mypost->website).'</a></p>';
+                                    if(!empty($mypost->website)) echo '<p><i class="ionicon ion-ios-world-outline" aria-hidden="true"></i><span>'.removeScheme($mypost->website).'</span></p>';
                                     if(!empty($mypost->address)) echo '<p><i class="ionicon ion-ios-navigate-outline" aria-hidden="true"></i><span>'.$mypost->address.'</span></p>';
                                 }
                             ?>
@@ -42,7 +42,12 @@
                 <div class="col-md-12 col-xs-12 excerpt-container">
                     <?php echo apply_filters( 'the_excerpt', $post->post_excerpt ); ?>
                     <div>
-                        <a class="btn btn-primary" href="<?php echo get_permalink() ?>" style="float: right; margin-bottom: 15px;">更多详情...</a>
+                        <?php 
+                            $pt=get_post_meta( $post->ID, '_wp_page_template', true );
+                            if($pt&&$pt!='default'):
+                        ?>
+                            <a class="btn btn-primary" href="<?php echo get_permalink() ?>" style="float: right; margin-bottom: 15px;">更多详情...</a>
+                        <?php endif; ?>
                         <?php include 'enquiry-form.php'; ?>
                     </div>
                 </div>
