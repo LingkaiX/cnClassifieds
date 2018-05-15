00066156c648522e234a0781f5815d37d0df8179
tvadsad <38453107+tvadsad@users.noreply.github.com>
2018-05-15 12:39:06 +1000

    add-advertise-page

---
+++ page-advertise.php
@@ -0,0 +1,111 @@
+<?php get_header(); ?>
+<style>
+    body{
+        min-width: 1140px;
+    }
+    .sect-2{
+        margin-top: 250px;
+        padding-left: 115px;
+        margin-bottom: 70px;
+    }
+    .sect-3{
+        padding-left: 115px;
+        background-color: #F6F6F6;
+        padding-bottom: 50px;
+    }
+    .sect-4{
+        padding-left: 115px;
+        background-color: rgba(23,192,169,0.7);
+    }
+    .background{
+        min-width: 1140px;
+        padding-left: 115px;
+        padding-top: 36px;
+        background-color: rgba(255,99,99,0.9); 
+        min-width:100%; 
+        height:613px;
+    }
+    .container{
+        padding-left: 30px;
+    }
+    @media (max-width:991px) {
+        .container{
+            width: 970px;
+        }
+        
+    }
+    /*@media(Max-width:768px){
+        .right{
+            color: #FFFFFF;
+        }
+    }*/
+    P{
+        padding-left: 3px;
+    }
+</style>
+<div class="sect-1">
+    <div class="background" >
+        <div class="container" style="">
+            <p style="font-size: 44px;font-family: PingFangTC-Semibold;margin-bottom: 0; color: #FFFFFF;">ADVERTISE</p>
+            <p style="font-size: 18px;font-family: PingFangTC-Regular; margin-bottom: 0; color: #FFFFFF;">Access to highly targeted AUADS audience</p>
+            <div>
+                <div  class="col-md-6 col-sm-6 col-xs-12 " style="float:left; width: 560px; padding-right:0px; padding-left: 0px; padding-top: 40px;">
+                    <img style ="" src="<?php echo get_template_directory_uri();?>/img/assets/basic listing sample.png"></img>
+                </div> 
+                <div class="col-md-6 col-sm-6 col-xs-12 "style=" width: 360px; padding-left: 100px; padding-top: 30px;" >
+                    <h3 class="right" style="font-size: 36px; letter-spacing: 3px;font-family: PingFangTC-Regular; margin-bottom: 0; color: #FFFFFF;">List Options</h3></br></br>
+                    <span style="font-size: 25px; letter-spacing: 3px;font-family:PingFangTC-Medium; color: #FFFFFF;">|</span>
+                    <span style="font-size: 19px; letter-spacing: 3px; font-family: PingFangTC-Light; color: #FFFFFF;">BASIC LISTING</span>
+                    <P style="font-size: 18px; letter-spacing: 3px; font-family: PingFangTC-Light; margin-top: 15px; margin-bottom: 15px; color: #FFFFFF;">&nbspPREMIUM LISTING</P>
+                    <P style="font-size: 18px; letter-spacing: 3px; font-family: PingFangTC-Light; margin-bottom: 15px; color: #FFFFFF;">&nbspPROFESSIONAL</P>
+                    <a style="display: inline-block; width: 240px; padding: 12px; background: #FFFFFF; color: rgba(255,99,99,0.9); text-align: center;
+                    border-radius: 5px; margin-top: 24px; " href="https://auads.com.au/publish-business-en">Get Free Ad</a>
+                </div>  
+            </div>
+        </div>
+    </div>
+</div>
+<div class="sect-2">
+    <div class="container" style=" padding-top: 50px;" >
+        <p style="font-size: 24px; font-weight: 550; font-family: PingFangTC-Medium; margin-bottom: 15px; color: #A90000;">Promote your business using AUADS templates</p>
+        <div class="col-md-6 col-sm-6 col-xs-12 " style="float:left; width: 340px; padding-right:0px; padding-left: 0px; padding-top: 40px;">
+            <P style="font-size: 18px;  font-family: PingFangTC-Regular; margin-bottom: 20px; color: #4A4A4A;">
+            AUADS provides basic templates for easy-to-use digital promotion for your business. Choose the most suitable for you and customise your own web page. 
+            This is the fastest and most efficient way to make your mark on the world wide web.</P>
+            <P style="font-size: 18px;  font-family: PingFangTC-Regular; margin-bottom: 20px; color: #4A4A4A;">Make your business more reputable.</P>
+            <P style="font-size: 18px;  font-family: PingFangTC-Regular; margin-bottom: 20px; color: #4A4A4A;">
+            Further customised options available so you have your own unique website.</P>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-12 "style=" width: 580px; padding-left: 90px; padding-top: 40px;" >
+            <img style ="padding-left: 20px;" src="<?php echo get_template_directory_uri();?>/img/assets/Cards.png"></img>
+        </div>
+    </div>
+</div>
+<div class="sect-3">
+    <div class="container" style=" padding-top: 50px;">
+        <p style="font-size: 36px; font-family: PingFangTC-Regular; margin-bottom: 30px; color: #A90000;">Homepage List</p>
+        <P style="font-size: 18px;  font-family: PingFangTC-Regular; margin-bottom: 50px; color: #333333 ;">
+        Become the recommended business on the home page to increase your exposure.</P>
+        <img style ="padding-left: 50px;" src="<?php echo get_template_directory_uri();?>/img/assets/Homepage list sample.png"></img>
+    </div>
+</div>
+<div class="sect-4">
+    <div class="container" style=" padding-top: 50px;">
+        <p style="font-size: 36px; font-family: PingFangTC-Regular; margin-bottom: 10px; color: white;">Rank to Top</p>
+        <P style="font-size: 18px;  font-family: PingFangTC-Regular; margin-bottom: 45px; color: white ;">
+        Maximise exposure to your targeted audience</P>
+        <div class="col-md-6 col-sm-6 col-xs-12 " style="float:left; width: 340px; padding-right:0px; padding-left: 0px; padding-top: 0px;">
+            <P style="font-size: 18px; letter-spacing: 0.72px; font-family: PingFangTC-Regular; margin-bottom: 30px; line-height: 30px; color: #FFFFFF;">
+            AUADS can help your business to be always ranked on top within the category pages or header page.</P>
+            <P style="font-size: 18px; letter-spacing: 0.72px; font-family: PingFangTC-Medium; margin-bottom: 15px; color: #FFFFFF;">What's included:</P>
+            <P style="font-size: 18px; letter-spacing: 0.72px; font-family: PingFangTC-Regular; margin-bottom: 10px; color:#FFFFFF;">   &nbsp&nbsp• 	Category page top position</P>
+            <P style="font-size: 18px; letter-spacing: 0.72px; font-family: PingFangTC-Regular; margin-bottom: 15px; color:#FFFFFF;">   &nbsp&nbsp• 	Multiple impressions</P>
+            <a style="display: inline-block; width: 240px; padding: 12px; background: white; color: rgba(23,192,169,0.7); text-align: center;
+                    border-radius: 5px; margin-top: 24px; " href="https://auads.com.au/publish-business-en">Start Your Ad</a>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-12 "style=" width: 580px; padding-left: 90px; padding-top: 85px;" >
+            <img style ="padding-left: 20px;" src="<?php echo get_template_directory_uri();?>/img/assets/Rank to top sample.png"></img>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
