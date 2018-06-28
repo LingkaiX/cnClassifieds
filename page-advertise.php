8fb575e4ab65ba1355178fa3b217e0a17644ef2b
tvadsad <38453107+tvadsad@users.noreply.github.com>
2018-06-28 11:21:48 +1000

    combine  2 rel=" "

--- page-advertise.php
+++ page-advertise.php
@@ -189,7 +189,7 @@ if (document.documentElement.clientWidth < 769) {
                     <button id="b2" class="list-potion" onclick="Click(2)" style="margin-bottom: 15px;">PREMIUM LISTING</button>
                     </br><span id="line-3" style="display: none; font-size: 25px; letter-spacing: 3px;font-family:PingFangTC-Medium; color: #FFFFFF;">|</span>
                     <button id="b3" class="list-potion" onclick="Click(3)" style="margin-bottom: 15px;">PROFESSIONAL</button>
-                    <a class="butt" target="_blank" rel="noopener" rel="nofollow" href="https://auads.com.au/pricelist#<?php echo 1; ?>">Get Free Ad</a>
+                    <a class="butt" target="_blank" rel="noopener nofollow" href="https://auads.com.au/pricelist#<?php echo 1; ?>">Get Free Ad</a>
                 </div>  
             </div>
         </div>
@@ -318,7 +318,7 @@ if (document.documentElement.clientWidth < 769) {
             <P style="font-size: 18px; letter-spacing: 0.72px; font-family: PingFangTC-Medium; margin-bottom: 15px; color: #FFFFFF;">What's included:</P>
             <P style="font-size: 18px; letter-spacing: 0.72px; font-family: PingFangTC-Regular; margin-bottom: 10px; color:#FFFFFF;">   &nbsp&nbsp• 	Category page top position</P>
             <P style="font-size: 18px; letter-spacing: 0.72px; font-family: PingFangTC-Regular; margin-bottom: 15px; color:#FFFFFF;">   &nbsp&nbsp• 	Multiple impressions</P>
-            <a class="butt2" target="_blank" rel="noopener" rel="nofollow" href="https://auads.com.au/pricelist#<?php echo 2; ?>">Start Your Ad</a>
+            <a class="butt2" target="_blank" rel="noopener nofollow" href="https://auads.com.au/pricelist#<?php echo 2; ?>">Start Your Ad</a>
         </div>
         <div class="col-md-6 col-sm-6 col-xs-12 "style=" width: 580px; padding-left: 90px; padding-top: 85px;" >
             <img style ="padding-left: 20px;" src="<?php echo get_template_directory_uri();?>/img/assets/Rank to top sample.png"></img>
